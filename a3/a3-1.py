e34f4e75a291480696183e2945e6a53a377380af
Dennis Ritter <dennis.r.ritter@gmail.com>
2018-01-15 15:05:41 +0100

    3.3

--- a3/a3-1.py
+++ a3/a3-1.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 ##### A3-1 #####
 P = 467
@@ -36,10 +37,18 @@ Q = 11
 E = 3
 
 
+def x_euklid(a, b):
+    if b == 0:
+        return (a, 1, 0)
+    (d2, s2, t2) = x_euklid(b, a % b)
+    (d, s, t) = (d2, t2, s2 - (a / b) * t2)
+    return d, s, t
+
+
 def encrypt_rsa(e, p, q, m):
     n = p * q
     y = (m ** e) % n
-    print('Verschlüsselung: n = {0}, y = {1}'.format(n, y))
+    print('Verschluesselung: n = {0}, y = {1}'.format(n, y))
     return y
 
 
@@ -48,9 +57,11 @@ def decrypt_rsa(e, p, q, y):
     phiN = (p-1) * (q-1)
     # (e * d) % phiN = 1
     # 3 * 107 = 321 = phiN * 8 + 1
-    d = 107
+    d = x_euklid(phiN, e)[2]
+    while d < 0:
+        d += phiN
     m = (y ** d) % n
-    print('Entschlüsselung: n = {0}, phiN = {1} d = {2}, m = {3}'.format(n, phiN, d, m))
+    print('Entschluesselung: n = {0}, phiN = {1} d = {2}, m = {3}'.format(n, phiN, d, m))
 
 
 decrypt_rsa(E, P, Q, encrypt_rsa(E, P, Q, M))
@@ -59,3 +70,16 @@ decrypt_rsa(E, P, Q, encrypt_rsa(E, P, Q, M))
 print('################')
 print('##### A3-3 #####')
 print('################')
+
+
+def calcD(p, q, e):
+    n = p * q
+    phiN = (p - 1) * (q - 1)
+    d = x_euklid(phiN, e)
+    print('n = {0}; phiN = {1};'.format(n, phiN))
+    print('Für p = {0}, q = {1} und e = {2}'.format(p, q, e))
+    print('..ist n = {0}, phiN = {1};'.format(n, phiN))
+    print('..ist der private Exponent d = {}'.format(d[2]))
+
+
+calcD(41, 17, 39)
